6bd34ca3bb586c85b3ea5de0c3f083c5e244afce
Juandamb <38229434+JuanDaMB@users.noreply.github.com>
2023-05-16 22:09:53 -0500

    Map 1 puzzle and exit

--- Assets/Scripts/CaveGameManager.cs
+++ Assets/Scripts/CaveGameManager.cs
@@ -1,18 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CaveGameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<Statue> Statues;
+    public List<int> puzzle;
+    private List<int> pressedLevers;
+
+    public bool levelComplete = false;
+    private int currentlevers;
+    public int maxLever;
+
+    public GameObject portal;
+    public Material leverRight, leverWrong;
     void Start()
     {
+        pressedLevers = new List<int>();
+        for (int i = 0; i < Statues.Count; i++)
+        {
+            Statues[i].SusbcribeID(i, LeverPulled);
+        }
+
+        levelComplete = false;
+    }
+    
+    public void LeverPulled(int value)
+    {
+        if (levelComplete)
+        {
+            return;
+        }
+        pressedLevers.Add(value);
+        currentlevers++;
         
+        if (currentlevers >= maxLever)
+        {
+            bool valid = true;
+
+            foreach (int i in pressedLevers)
+            {
+                if (!puzzle.Contains(i))
+                {
+                    valid = false;
+                }
+            }
+
+            if (valid)
+            {
+                foreach (Statue statue in Statues)
+                {
+                    statue.AssignMaterial(leverRight);
+                }
+                portal.SetActive(true);
+                levelComplete = true;
+            }
+            else
+            {
+                foreach (Statue statue in Statues)
+                {
+                    statue.ResetLevers(leverWrong);
+                }
+                pressedLevers.Clear();
+                currentlevers = 0;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void OnDestroy()
     {
-        
+        foreach (Statue statue in Statues)
+        {
+            statue.Clean();
+        }
     }
 }
---
+++ Assets/Scripts/Statue.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Tilia.Interactions.Controllables.AngularDriver;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Statue : MonoBehaviour
+{
+    public int id;
+    public Material OnPulled;
+    private Action<int> OnPulledInt;
+    public AngularDriveFacade facade;
+    public MeshRenderer leverRenderer;
+    private bool Pulled = false;
+
+    public void SusbcribeID(int ID, Action<int> callback)
+    {
+        Pulled = false;
+        id = ID;
+        OnPulledInt = callback;
+    }
+    
+    public void Activate()
+    {
+        Pulled = true;
+        leverRenderer.sharedMaterial = OnPulled;
+        OnPulledInt?.Invoke(id);
+    }
+
+
+    public void Clean()
+    {
+        OnPulledInt = null;
+    }
+
+    public void AssignMaterial(Material material)
+    {
+        leverRenderer.sharedMaterial = material;
+    }
+
+    public void ResetLevers(Material material)
+    {
+        if (Pulled)
+        {
+            leverRenderer.sharedMaterial = material;
+        }
+
+        Pulled = false;
+        facade.TargetValue = 1;
+        facade.MoveToTargetValue = true;
+    }
+}
