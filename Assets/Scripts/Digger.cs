f578ea9cc8a90bba4fb667cbbbb78e7f1ca88a19
scarhhh3 <333carmor@gmail.com>
2023-05-26 04:07:22 -0500

    digger fix

--- Assets/Scripts/Digger.cs
+++ Assets/Scripts/Digger.cs
@@ -2,6 +2,7 @@ using Oculus.Interaction;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Digger : MonoBehaviour
@@ -11,16 +12,31 @@ public class Digger : MonoBehaviour
 
     Vector3 chestOriginalPos;
 
+    public bool canDig = true;
+
+    public int count;
+
     private void Start()
     {
         chestOriginalPos = chest.transform.position;
+
+        count = sandgroups.Length;
     }
 
-    private void OnCollisionExit(Collision collision)
+    ////private void OnCollisionExit(Collision collision)
+    ////{
+    ////    if (collision.gameObject.CompareTag("shovel"))
+    ////    {
+    ////        Debug.Log("coll exit");
+    ////        RemoveGroupSand();
+    ////        PullUpChest();
+    ////    }
+    ////}
+
+    private void OnTriggerExit(Collider other)
     {
-        if (collision.gameObject.CompareTag("shovel"))
+        if (canDig && other.gameObject.CompareTag("shovel"))
         {
-            //Debug.Log("coll exit");
             RemoveGroupSand();
             PullUpChest();
         }
@@ -36,12 +52,35 @@ public class Digger : MonoBehaviour
 
     private void RemoveGroupSand()
     {
-        foreach (var sand in sandgroups)
-        {
-            if (sand.active)
-            {
-                sand.gameObject.SetActive(false);
-            }
-        }
+        //foreach (var sand in sandgroups)
+        //{
+        //    if (sand.active)
+        //    {
+        //        sand.gameObject.SetActive(false);
+        //    }
+        //}
+
+        canDig = false;
+        //count = sandgroups.Length;
+        sandgroups[count - 1].SetActive(false);
+        count--;
+
+        StartCoroutine(WaitCD());
+    }
+
+    IEnumerator WaitCD()
+    {
+        //Print the time of when the function is first called.
+        //Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //RemoveGroupSand();
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(2);
+
+        //After we have waited 5 seconds print the time again.
+        //Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+
+        canDig = true;
     }
 }
