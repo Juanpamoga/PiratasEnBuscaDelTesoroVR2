3c7848583e904494a00f44807befdd22927420bd
scarhhh3 <333carmor@gmail.com>
2023-05-26 02:00:58 -0500

    firecannon

---
+++ Assets/BallCharger.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCharger : MonoBehaviour
+{
+    private bool isReloaded;
+
+    public GameObject shoteableCannonBall;
+    public Transform barrel;
+
+    public float force;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.gameObject.tag);
+        if (other.gameObject.CompareTag("ball"))
+        {
+            isReloaded = true;
+        }
+
+        if (isReloaded && other.gameObject.CompareTag("Fire"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(shoteableCannonBall, barrel.position, barrel.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = barrel.forward * force * Time.deltaTime; ;
+
+        isReloaded = false;
+    }
+}
